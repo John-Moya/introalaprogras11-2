0c8a1d4c635d27b54cf0eff7f3d06cd8e909f8b9
Kenneth19920 <91219301+Kenneth19920@users.noreply.github.com>
2021-10-22 16:28:07 -0600

    Add files via upload

---
+++ Semana_4/src/Data/Medicamentos.java
@@ -0,0 +1,14 @@
+package Data;
+
+public class Medicamentos {
+    public int codigo;
+    public String nombre;
+    public String Fabricante;
+
+
+    public static void entregar(){
+        System.out.println("El medicamento ha sido entregado");
+
+    }
+
+}
